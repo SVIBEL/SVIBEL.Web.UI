d89bd878254b08982179eba0a132bf009e7cd38a
Peter Svitz <peter.svitz@gmail.com>
2017-02-18 22:54:48 -0500

    mobile

--- src/components/MainHero.js
+++ src/components/MainHero.js
@@ -6,16 +6,16 @@ const MainHero = () =>
             <div className="row">
                 
 
-                <div className="col-xs-12 col-sm-6">
+                <div className="col-sm-6">
                     <img className="img-responsive hero-profile" src="../../media/peter.JPG"/>
                 </div>
-                <div className="col-xs-12 col-sm-6">
+                <div className="col-sm-6">
                     <img className="img-responsive hero-profile" src="../../media/akos.JPG"/>
                 </div>
                 
                 <div className="container brand">
                     <div className="col-sm-12">
-                            <img className="img-logo" src='../../media/logo.png' />
+                            <img className="img-responsive img-logo" src='../../media/logo.png' />
                     </div>
 
                     <div className="col-md-2 col-md-offset-5">
